7c351f9afe373e91b558a1298ffb62fa1b204bd2
Binh - Soft <Binh - Soft@PMCW0881>
2023-04-04 13:22:28 +0700

    aa

--- src/main/java/main/MainTesting.java
+++ src/main/java/main/MainTesting.java
@@ -2,7 +2,7 @@ package main;
 
 import testCase.checkLabel;
 
-public class mainTesting {
+public class MainTesting {
 	
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "F:\\automation\\New folder\\chromedriver.exe");
